7a3b8cd5f351043fbc9968a6d3806e072890e72e
Luca Valsecchi <luca.valsecchi@noku.io>
2024-02-21 15:46:53 +0100

    chore(dep): update deps

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,29 +1,31 @@
 module.exports = {
-  "env": {
-    "node": true,
-    "commonjs": true,
-    "browser": true,
-    "es6": true
-  },
-  "extends": [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended"
-  ],
-  "overrides": [
-    {
-      "files": ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"]
-    }
-  ],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
-    },
-    "ecmaVersion": "latest",
-    "sourceType": "module"
-  },
-  "plugins": ["@typescript-eslint", "react"],
-  "rules": {}
+	env: {
+		node: true,
+		commonjs: true,
+		browser: true,
+		es6: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react/recommended',
+		'plugin:react-hooks/recommended',
+	],
+	overrides: [
+		{
+			files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+		},
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['@typescript-eslint', 'react'],
+	rules: {
+		'react/react-in-jsx-scope': 'off',
+	},
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{js,ts,jsx,tsx}', "./index.html",],
-  plugins: [require('@tailwindcss/typography'), require('daisyui')],
-  theme: {
-    extend: {},
-  },
+	content: ['./src/**/*.{js,ts,jsx,tsx}', './index.html'],
+	plugins: [require('@tailwindcss/typography'), require('daisyui')],
+	theme: {
+		extend: {},
+	},
 }
-
