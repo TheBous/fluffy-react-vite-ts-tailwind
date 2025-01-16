dae00982049809913026912a4a51cc9f68570574
TheBous <thebous1993@gmail.com>
2025-01-16 20:21:53 +0100

    feat(ts): Update eslint.config.js for ts support and eslint 9

--- eslint.config.js
+++ eslint.config.js
@@ -1,105 +1,110 @@
-
-import pluginJs from "@eslint/js";
-import importConfig from "eslint-plugin-import";
-import jest from "eslint-plugin-jest";
-import jsdocConfig from "eslint-plugin-jsdoc";
-import jsoncConfig from "eslint-plugin-jsonc";
-import reactHooks from "eslint-plugin-react-hooks";
-import pluginReactAllConfig from "eslint-plugin-react/configs/all.js";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-import sortKeysFixConfig from "eslint-plugin-sort-keys-fix";
-import spellcheck from "eslint-plugin-spellcheck";
+// eslint.config.js
+import js from "@eslint/js";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import importPlugin from "eslint-plugin-import";
+import jestPlugin from "eslint-plugin-jest";
+import jsdocPlugin from "eslint-plugin-jsdoc";
+import jsoncPlugin from "eslint-plugin-jsonc";
+import reactPlugin from "eslint-plugin-react";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
+import sortKeysFixPlugin from "eslint-plugin-sort-keys-fix";
+import spellcheckPlugin from "eslint-plugin-spellcheck";
 import globals from "globals";
 
 export default [
     {
-        "ignores": ["**/dist/**", "src/main.tsx"]
+        ignores: ["**/dist/**", "src/main.tsx"],
     },
-    pluginJs.configs.recommended,
+    js.configs.recommended,
     {
-        "files": ["**/*.{ts,tsx}"],
-        ...jest.configs["flat/recommended"],
-        "languageOptions": {
-            "globals": {
+        files: ["**/*.{ts,tsx}"],
+        languageOptions: {
+            parser: tsParser,
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+                project: "./tsconfig.json",
+            },
+            globals: {
                 ...globals.browser,
                 ...globals.node,
-                "describe": "readonly",
-                "expect": "readonly",
-                "indexedDB": "readonly",
-                "it": "readonly",
-                "jest": "readonly",
-                "test": "readonly"
+                describe: "readonly",
+                expect: "readonly",
+                indexedDB: "readonly",
+                it: "readonly",
+                jest: "readonly",
+                test: "readonly",
             },
-            "parserOptions": {
-                "ecmaFeatures": {
-                    "jsx": true
-                },
-                "requireConfigFile": false
-            }
         },
-        "plugins": {
-            "import": importConfig,
-            jest,
-            "jsdoc": jsdocConfig,
-            "jsonc": jsoncConfig,
-            "react": { ...pluginReactAllConfig, ...pluginReactConfig },
-            "react-hooks": reactHooks,
-            "sort-keys-fix": sortKeysFixConfig,
-            "spellcheck": spellcheck
+        plugins: {
+            import: importPlugin,
+            jest: jestPlugin,
+            jsdoc: jsdocPlugin,
+            jsonc: jsoncPlugin,
+            react: reactPlugin,
+            "react-hooks": reactHooksPlugin,
+            "sort-keys-fix": sortKeysFixPlugin,
+            spellcheck: spellcheckPlugin,
+            "@typescript-eslint": tsPlugin,
         },
-        "rules": {
+        rules: {
             "array-bracket-newline": ["error", "consistent"],
             "array-bracket-spacing": ["error", "never"],
             "array-callback-return": "error",
             "array-element-newline": [
                 "error",
                 {
-                    "ArrayExpression": "consistent",
-                    "ArrayPattern": { "minItems": 3 }
-                }
+                    ArrayExpression: "consistent",
+                    ArrayPattern: { minItems: 3 },
+                },
             ],
             "arrow-spacing": "error",
             "brace-style": ["error", "1tbs"],
-            "camelcase": ["error", { "ignoreDestructuring": true, "properties": "never" }],
+            camelcase: ["error", { ignoreDestructuring: true, properties: "never" }],
             "comma-dangle": ["error", "never"],
             "comma-spacing": [
                 "error",
                 {
-                    "after": true,
-                    "before": false
-                }
+                    after: true,
+                    before: false,
+                },
             ],
-            "curly": ["error", "all"],
+            curly: ["error", "all"],
             "default-case": "off",
-            "eqeqeq": ["error", "always"],
+            eqeqeq: ["error", "always"],
             "func-call-spacing": ["error", "never"],
             "function-call-argument-newline": ["error", "consistent"],
             "import/first": "off",
-            "import/order": ["error", { "groups": ["builtin", "external", "internal", "parent", "sibling", "index"] }],
-            "indent": ["error", "tab", { "SwitchCase": 1, "ignoredNodes": ["ConditionalExpression"] }],
+            "import/order": [
+                "error",
+                { groups: ["builtin", "external", "internal", "parent", "sibling", "index"] },
+            ],
+            indent: ["error", "tab", { SwitchCase: 1, ignoredNodes: ["ConditionalExpression"] }],
             "jest/no-mocks-import": "off",
-            "jsdoc/newline-after-description": 0, // Required for vs code auto formatting
+            "jsdoc/newline-after-description": 0, // Required for VS Code auto formatting
             "jsdoc/require-hyphen-before-param-description": 1,
             "jsdoc/require-jsdoc": [
                 "error",
                 {
-                    "require": {
-                        "ArrowFunctionExpression": true,
-                        "ClassExpression": true,
-                        "FunctionDeclaration": true,
-                        "FunctionExpression": true,
-                        "MethodDefinition": true
-                    }
-                }
+                    require: {
+                        ArrowFunctionExpression: true,
+                        ClassExpression: true,
+                        FunctionDeclaration: true,
+                        FunctionExpression: true,
+                        MethodDefinition: true,
+                    },
+                },
             ],
             "jsonc/sort-keys": "error",
-            "keyword-spacing": ["error", { "after": true, "before": true }],
+            "keyword-spacing": ["error", { after: true, before: true }],
             "max-len": [
                 "error",
                 {
-                    "code": 120,
-                    "ignorePattern": "\".*\": \".*\"" // Ignore pattern for strings in json as they can't be broken in multi lines
-                }
+                    code: 120,
+                    ignorePattern: '".*": ".*"', // Ignore pattern for strings in JSON as they can't be broken into multiple lines
+                },
             ],
             "no-console": "error",
             "no-dupe-else-if": "error",
@@ -107,64 +112,62 @@ export default [
             "no-nested-ternary": "error",
             "no-useless-escape": "off",
             "no-var": "error",
-            "object-curly-newline": ["error", { "consistent": true, "multiline": true }],
+            "object-curly-newline": ["error", { consistent: true, multiline: true }],
             "object-curly-spacing": ["error", "always"],
-            "object-property-newline": ["error", { "allowAllPropertiesOnSameLine": true }],
+            "object-property-newline": ["error", { allowAllPropertiesOnSameLine: true }],
             "padding-line-between-statements": [
                 "error",
                 // Always one empty line before return statement
                 {
-                    "blankLine": "always",
-                    "next": "return",
-                    "prev": "*"
+                    blankLine: "always",
+                    next: "return",
+                    prev: "*",
                 },
                 // Always one empty line between methods
                 {
-                    "blankLine": "always",
-                    "next": ["block-like", "multiline-block-like"],
-                    "prev": ["block-like", "multiline-block-like"]
+                    blankLine: "always",
+                    next: ["block-like", "multiline-block-like"],
+                    prev: ["block-like", "multiline-block-like"],
                 },
-                // Avoids more than one empty line
+                // Avoid more than one empty line
                 {
-                    "blankLine": "never",
-                    "next": "empty",
-                    "prev": "empty"
-                }
+                    blankLine: "never",
+                    next: "empty",
+                    prev: "empty",
+                },
             ],
             "prefer-const": "error",
-            "quotes": ["error", "double"],
-            "radix": "off",
+            quotes: ["error", "double"],
+            radix: "off",
             "react-hooks/exhaustive-deps": "error",
             "react-hooks/rules-of-hooks": "error",
             "react/react-in-jsx-scope": "off",
-            "semi": ["error", "always"],
+            semi: ["error", "always"],
             "sort-keys": [
                 "error",
                 "asc",
                 {
-                    "caseSensitive": true,
-                    "minKeys": 2,
-                    "natural": false
-                }
+                    caseSensitive: true,
+                    minKeys: 2,
+                    natural: false,
+                },
             ],
             "sort-keys-fix/sort-keys-fix": "error",
             "space-before-blocks": "error",
-            "space-infix-ops": ["error", { "int32Hint": false }],
+            "space-infix-ops": ["error", { int32Hint: false }],
             "spellcheck/spell-checker": [
                 "error",
                 {
-                    "minLength": 4,
-                    "skipWords": [
-
-                        "datasource",
-                        "apis"
-                    ]
-                }
+                    minLength: 4,
+                    skipWords: ["datasource", "apis"],
+                },
             ],
-            // Added this so test file don't give error when accessing dom element using DOM api
-            "testing-library/no-node-access": "off"
+            // Added this so test files don't give error when accessing DOM elements using DOM API
+            "testing-library/no-node-access": "off",
+            // TypeScript-specific rules
+            "@typescript-eslint/no-unused-vars": "error",
+            "@typescript-eslint/explicit-function-return-type": "off",
         },
-        "settings": { "react": { "version": "detect" } }
-    }
+        settings: { react: { version: "detect" } },
+    },
 ];
-
