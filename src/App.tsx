a32819b9bb51233640446398b09aa367d93d0283
TheBous <thebous1993@gmail.com>
2025-06-27 14:20:30 +0200

    feat(merge): merge feat/tailwind [squash]

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ const App = () => {
 	return (
 		<div className="w-screen h-screen flex items-center justify-center">
 			<span className="font-bold text-2xl">🫶 React Typescript boilerplate</span>
+			<button className="btn btn-secondary">Secondary</button>
+
 		</div>
 	);
 };
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-	content: ["./src/**/*.{js,ts,jsx,tsx}", "./index.html"],
-	// eslint-disable-next-line no-undef
-	plugins: [require("@tailwindcss/typography")],
-	theme: {
-		extend: {}
-	}
-};
