60f3809b82e463b284f1e02094314be13e39f6af
TheBous <thebous1993@gmail.com>
2025-06-27 14:18:55 +0200

    chore(deps): add @tailwindcss/vite, autoprefixer, and daisyui dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ const App = () => {
 	return (
 		<div className="w-screen h-screen flex items-center justify-center test">
 			<span className="font-bold text-2xl">🫶 React Typescript boilerplate</span>
+			<button className="btn btn-secondary">Secondary</button>
+
 		</div>
 	);
 };
